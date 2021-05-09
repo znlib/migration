2536cc5f3ee0161d87a5782cf5e72c50afc02cb5
Vitaliy <theyamshikov@yandex.ru>
2021-05-09 17:14:49 +0600

    upd

--- src/Commands/config/consoleBootstrap.php
+++ src/Commands/config/consoleBootstrap.php
@@ -1,24 +1,17 @@
 <?php
 
-use Illuminate\Container\Container;
-use ZnCore\Base\Libs\App\Loaders\BundleLoader;
-use ZnLib\Db\Capsule\Manager;
-use ZnCore\Domain\Interfaces\Libs\EntityManagerInterface;
-use Psr\Container\ContainerInterface;
-use ZnCore\Domain\Libs\EntityManager;
-use ZnCore\Base\Libs\DotEnv\DotEnv;
-use ZnLib\Db\Factories\ManagerFactory;
 use ZnCore\Base\Libs\App\Kernel;
+use ZnCore\Base\Libs\App\Loaders\BundleLoader;
+use ZnCore\Base\Libs\DotEnv\DotEnv;
 
 DotEnv::init();
 
 $kernel = new Kernel('console');
-$container = Container::getInstance();
-$kernel->setContainer($container);
+$container = $kernel->getContainer();
 $bundleLoader = new BundleLoader([], ['i18next', 'container', 'console', 'migration']);
 $appBundlesConfigFile = \ZnCore\Base\Legacy\Yii\Helpers\FileHelper::path($_ENV['BUNDLES_CONFIG_FILE']);
 
-if(file_exists($appBundlesConfigFile)) {
+if (file_exists($appBundlesConfigFile)) {
     $bundleLoader->addBundles(include $appBundlesConfigFile);
 }
 $bundleLoader->addBundles(include __DIR__ . '/bundle.php');
